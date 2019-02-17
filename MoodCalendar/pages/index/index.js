61bd7bddf4295614903fe6c6ee813ab8e6e2c825
winhows5 <wangwh258@gmail.com>
2019-02-17 18:46:30 +0800

    functions

--- MoodCalendar/pages/index/index.js
+++ MoodCalendar/pages/index/index.js
@@ -3,9 +3,7 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-  },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -19,6 +17,40 @@ Page({
       title: '定制我的心情日历',
       path: '/pages/index/index'
     }
+  },
+
+    onShow() {
+    wx.reportAnalytics('enter_home_programmatically', {})
+  },
+  onShareAppMessage() {
+    return {
+      title: '小程序官方组件展示',
+      path: 'page/component/index'
+    }
+  },
+
+  data: {
+    location: "北京",
+    weather: "多云",
+    
+  },
+
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for(let i = 0, len = list.length; i<len; ++i) {
+  if (list[i].id === id) {
+    list[i].open = !list[i].open
+  } else {
+    list[i].open = false
   }
+}
+this.setData({
+  list
+})
+wx.reportAnalytics('click_view_programmatically', {})
+  }
+
+
 
 })
